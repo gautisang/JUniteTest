3c3af8648e5b14eb5ae96e224878f0f092f81802
gautamnarnolia <gautisang@gmail.com>
2023-05-05 12:26:49 +0530

    Initial Commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.junittest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
