2388c149c57d23b8ecd6638fc7065135c25e3813
gautamnarnolia <gautisang@gmail.com>
2023-05-05 15:16:37 +0530

    Added DisplayName annnotation use

--- src/test/java/DemoAnnotation.java
+++ src/test/java/DemoAnnotation.java
@@ -1,30 +1,27 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
-public class DemoTest {
-
-    // Test Name naming Convention
-    // test<System Under Test>_<Condition or State Change>_<Expectred Result>
-
-    @Test
-    void testDemo(){
-        System.out.println("Test Demo");
-    }
+@DisplayName("Test Math Opearation")
+public class DemoAnnotation {
 
     @Test
+    @DisplayName("Test 4/2 = 2")
     void testIntegerDivision_WhenFourDividedByTwo_ShouldReturnTwo() {
         Calculator calculator = new Calculator();
         int result =  calculator.integerDivision(4, 2);
         assertEquals(2, result, "4/2 did not produce 2");
     }
 
+    @DisplayName("Divided By Zero")
     @Test
     void testIntegerDivision_WhenrDividedZero_ShouldThrowArithmaticException() {
-
+        fail("Not Implemented");
     }
 
-
+    @DisplayName("Test 33-1=32")
     @Test
     void integerSubtraction() {
         Calculator calculator = new Calculator();
